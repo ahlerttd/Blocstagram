ab94030d1c6d4e70888cfcbddc8160ad585d3b13
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-08-22 15:41:06 -0400

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Trevor Ahlert on 8/22/14.
+//  Copyright (c) 2014 Trevor Ahlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
