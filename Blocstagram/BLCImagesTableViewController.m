a5ead9acb812aa03e7a7788e83180bf30f859b69
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-08-31 12:21:19 -0400

    KVO and swipe to delete added

--- Blocstagram/BLCImagesTableViewController.m
+++ Blocstagram/BLCImagesTableViewController.m
@@ -41,6 +41,8 @@
 {
     [super viewDidLoad];
     
+    [[BLCDataSource sharedInstance] addObserver:self forKeyPath:@"mediaItems" options:0 context:nil];
+    
     [self.tableView registerClass:[BLCMediaTableViewCell class] forCellReuseIdentifier:@"mediaCell"];
     
 }
@@ -49,6 +51,53 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) dealloc
+{
+    [[BLCDataSource sharedInstance] removeObserver:self forKeyPath:@"mediaItems"];
+}
+
+- (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (object == [BLCDataSource sharedInstance] && [keyPath isEqualToString:@"mediaItems"]) {
+        // We know mediaItems changed.  Let's see what kind of change it is.
+        int kindOfChange = [change[NSKeyValueChangeKindKey] intValue];
+        
+        if (kindOfChange == NSKeyValueChangeSetting) {
+            // Someone set a brand new images array
+            [self.tableView reloadData];
+        }else if (kindOfChange == NSKeyValueChangeInsertion ||
+               kindOfChange == NSKeyValueChangeRemoval ||
+               kindOfChange == NSKeyValueChangeReplacement) {
+        // We have an incremental change: inserted, deleted, or replaced images
+        
+        // Get a list of the index (or indices) that changed
+        NSIndexSet *indexSetOfChanges = change[NSKeyValueChangeIndexesKey];
+        
+        // Convert this NSIndexSet to an NSArray of NSIndexPaths (which is what the table view animation methods require)
+        NSMutableArray *indexPathsThatChanged = [NSMutableArray array];
+        [indexSetOfChanges enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
+            NSIndexPath *newIndexPath = [NSIndexPath indexPathForRow:idx inSection:0];
+            [indexPathsThatChanged addObject:newIndexPath];
+        }];
+        
+        // Call `beginUpdates` to tell the table view we're about to make changes
+        [self.tableView beginUpdates];
+        
+        // Tell the table view what the changes are
+        if (kindOfChange == NSKeyValueChangeInsertion) {
+            [self.tableView insertRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+        } else if (kindOfChange == NSKeyValueChangeRemoval) {
+            [self.tableView deleteRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+        } else if (kindOfChange == NSKeyValueChangeReplacement) {
+            [self.tableView reloadRowsAtIndexPaths:indexPathsThatChanged withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+        
+        // Tell the table view that we're done telling it about changes, and to complete the animation
+        [self.tableView endUpdates];
+        }
+}
+
 }
 
 - (NSMutableArray *) items
@@ -93,52 +142,9 @@
     // If row is deleted, remove it from the list.
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         
-        [[self items] removeObjectAtIndex:indexPath.row];
-        [self.tableView deleteRowsAtIndexPaths:[NSMutableArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-        
-        
+        BLCMedia *item = [BLCDataSource sharedInstance].mediaItems[indexPath.row];
+        [[BLCDataSource sharedInstance] deleteMediaItem:item];
     }
 }
-
-
-
-
-/*
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-} */
-
-
-
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
