c62739278c11c4f4daf86f01e94ecd61170ffcb7
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-09-08 14:20:10 -0400

    Connect to instagram - login page not showing

---
+++ Blocstagram/BLCLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BLCLoginViewController.h
+//  Blocstagram
+//
+//  Created by Trevor Ahlert on 9/2/14.
+//  Copyright (c) 2014 Trevor Ahlert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCLoginViewController : UIViewController
+
+extern NSString *const BLCLoginViewControllerDidGetAccessTokenNotification;
+
+@end
---
+++ Blocstagram/BLCLoginViewController.m
@@ -0,0 +1,95 @@
+//
+//  BLCLoginViewController.m
+//  Blocstagram
+//
+//  Created by Trevor Ahlert on 9/2/14.
+//  Copyright (c) 2014 Trevor Ahlert. All rights reserved.
+//
+
+#import "BLCLoginViewController.h"
+#import "BLCDataSource.h"
+
+@interface BLCLoginViewController () <UIWebViewDelegate>
+
+@property (nonatomic, weak) UIWebView *webView;
+
+@end
+
+@implementation BLCLoginViewController
+
+NSString *const BLCLoginViewControllerDidGetAccessTokenNotification = @"BLCLoginViewControllerDidGetAccessTokenNotification";
+
+
+- (NSString *)redirectURI {
+    return @"<http://overlyhautz.com>";
+}
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)loadView {
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.delegate = self;
+    
+    self.webView = webView;
+    self.view = webView;
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [BLCDataSource instagramClientID], [self redirectURI]];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    if (url) {
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+        [self.webView loadRequest:request];
+    }
+}
+
+- (void) dealloc {
+    // Removing this line causes a weird flickering effect when you relaunch the app after logging in, as the web view is briefly displayed, automatically authenticates with cookies, returns the access token, and dismisses the login view, sometimes in less than a second.
+    [self clearInstagramCookies];
+    
+    // see https://developer.apple.com/library/ios/documentation/uikit/reference/UIWebViewDelegate_Protocol/Reference/Reference.html#//apple_ref/doc/uid/TP40006951-CH3-DontLinkElementID_1
+    self.webView.delegate = nil;
+}
+
+/**
+ Clears Instagram cookies. This prevents caching the credentials in the cookie jar.
+ */
+- (void) clearInstagramCookies {
+    for(NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]) {
+        NSRange domainRange = [cookie.domain rangeOfString:@"instagram.com"];
+        if(domainRange.location != NSNotFound) {
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+        }
+    }
+}
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    NSString *urlString = request.URL.absoluteString;
+    if ([urlString hasPrefix:[self redirectURI]]) {
+        // This contains our auth token
+        NSRange rangeOfAccessTokenParameter = [urlString rangeOfString:@"access_token="];
+        NSUInteger indexOfTokenStarting = rangeOfAccessTokenParameter.location + rangeOfAccessTokenParameter.length;
+        NSString *accessToken = [urlString substringFromIndex:indexOfTokenStarting];
+        [[NSNotificationCenter defaultCenter] postNotificationName:BLCLoginViewControllerDidGetAccessTokenNotification object:accessToken];
+        return NO;
+    }
+    
+    return YES;
+}
+
+@end
