227fcae03f337131d29c5a8d62b798fc5aeab5ce
TREVOR AHLERT <trevor.ahlert@gmail.com>
2014-09-12 09:28:37 -0400

    Fixed Pull to Refresh Bug

--- Blocstagram/BLCDataSource.m
+++ Blocstagram/BLCDataSource.m
@@ -13,7 +13,7 @@
 #import "BLCLoginViewController.h"
 
 @interface BLCDataSource () {
-  NSMutableArray *_mediaItems;
+    NSMutableArray *_mediaItems;
 }
 
 @property(nonatomic, strong) NSMutableArray *mediaItems;
@@ -21,140 +21,188 @@
 @property(nonatomic, assign) BOOL isRefreshing;
 @property(nonatomic, assign) BOOL isLoadingOlderItems;
 @property(nonatomic, strong) NSString *accessToken;
+@property(nonatomic, assign) BOOL thereAreNoMoreOlderMessages;
 
 @end
 
 @implementation BLCDataSource
 
 + (instancetype)sharedInstance {
-  static dispatch_once_t once;
-  static id sharedInstance;
-  dispatch_once(&once, ^{ sharedInstance = [[self alloc] init]; });
-  return sharedInstance;
+    static dispatch_once_t once;
+    static id sharedInstance;
+    dispatch_once(&once, ^{ sharedInstance = [[self alloc] init]; });
+    return sharedInstance;
 }
 
 - (instancetype)init {
-  self = [super init];
-
-  if (self) {
-      [self registerForAccessTokenNotification];
-  }
-
-  return self;
+    self = [super init];
+    
+    if (self) {
+        [self registerForAccessTokenNotification];
+    }
+    
+    return self;
 }
 
-- (void) registerForAccessTokenNotification {
-    [[NSNotificationCenter defaultCenter] addObserverForName:BLCLoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
-        self.accessToken = note.object;
-        
-        // Got a token, populate the initial data
-        [self populateDataWithParameters:nil];
-    }];
+- (void)registerForAccessTokenNotification {
+    [[NSNotificationCenter defaultCenter]
+     addObserverForName:BLCLoginViewControllerDidGetAccessTokenNotification
+     object:nil
+     queue:nil
+     usingBlock:^(NSNotification *note) {
+         self.accessToken = note.object;
+         
+         // Got a token, populate the initial data
+         [self populateDataWithParameters:nil completionHandler:nil];
+     }];
 }
 
 + (NSString *)instagramClientID {
-  return @"0a418c2844794c51ad46b3c5aabfa2e5";
+    return @"0a418c2844794c51ad46b3c5aabfa2e5";
 }
-
 
 #pragma mark - Key/Value Observing
 
 - (NSUInteger)countOfMediaItems {
-  return self.mediaItems.count;
+    return self.mediaItems.count;
 }
 
 - (id)objectInMediaItemsAtIndex:(NSUInteger)index {
-  return [self.mediaItems objectAtIndex:index];
+    return [self.mediaItems objectAtIndex:index];
 }
 
 - (NSArray *)mediaItemsAtIndexes:(NSIndexSet *)indexes {
-  return [self.mediaItems objectsAtIndexes:indexes];
+    return [self.mediaItems objectsAtIndexes:indexes];
 }
 
 - (void)insertObject:(BLCMedia *)object inMediaItemsAtIndex:(NSUInteger)index {
-  [_mediaItems insertObject:object atIndex:index];
+    [_mediaItems insertObject:object atIndex:index];
 }
 
 - (void)removeObjectFromMediaItemsAtIndex:(NSUInteger)index {
-  [_mediaItems removeObjectAtIndex:index];
+    [_mediaItems removeObjectAtIndex:index];
 }
 
 - (void)replaceObjectInMediaItemsAtIndex:(NSUInteger)index
                               withObject:(id)object {
-  [_mediaItems replaceObjectAtIndex:index withObject:object];
+    [_mediaItems replaceObjectAtIndex:index withObject:object];
 }
 
 - (void)deleteMediaItem:(BLCMedia *)item {
-  NSMutableArray *mutableArrayWithKVO =
-      [self mutableArrayValueForKey:@"mediaItems"];
-  [mutableArrayWithKVO removeObject:item];
+    NSMutableArray *mutableArrayWithKVO =
+    [self mutableArrayValueForKey:@"mediaItems"];
+    [mutableArrayWithKVO removeObject:item];
 }
 
 - (void)requestNewItemsWithCompletionHandler:
-            (BLCNewItemCompletionBlock)completionHandler {
-  if (self.isRefreshing == NO) {
-    self.isRefreshing = YES;
-    
-
-    self.isRefreshing = NO;
-
-    if (completionHandler) {
-      completionHandler(nil);
+(BLCNewItemCompletionBlock)completionHandler {
+    self.thereAreNoMoreOlderMessages = NO;
+    if (self.isRefreshing == NO) {
+        self.isRefreshing = YES;
+        
+        NSDictionary *parameters = @{};
+        
+        if (self.mediaItems.count != 0) {
+            NSString *minID = [[self.mediaItems firstObject] idNumber];
+            parameters = @{ @"min_id" : minID };
+        }
+        
+        [self populateDataWithParameters:parameters
+                       completionHandler:^(NSError *error) {
+                           self.isRefreshing = NO;
+                           
+                           if (completionHandler) {
+                               completionHandler(error);
+                           }
+                       }];
     }
-  }
 }
 
 - (void)requestOldItemsWithCompletionHandler:
-            (BLCNewItemCompletionBlock)completionHandler {
-  if (self.isLoadingOlderItems == NO) {
-    self.isLoadingOlderItems = YES;
-   
-    self.isLoadingOlderItems = NO;
-
-    if (completionHandler) {
-      completionHandler(nil);
+(BLCNewItemCompletionBlock)completionHandler {
+    if (self.isLoadingOlderItems == NO &&
+        self.thereAreNoMoreOlderMessages == NO) {
+        self.isLoadingOlderItems = YES;
+        
+        NSString *maxID = [[self.mediaItems lastObject] idNumber];
+        NSDictionary *parameters = @{ @"max_id" : maxID };
+        
+        [self populateDataWithParameters:parameters
+                       completionHandler:^(NSError *error) {
+                           self.isLoadingOlderItems = NO;
+                           
+                           if (completionHandler) {
+                               completionHandler(error);
+                           }
+                       }];
     }
-  }
 }
 
-- (void) populateDataWithParameters:(NSDictionary *)parameters {
+- (void)populateDataWithParameters:(NSDictionary *)parameters
+                 completionHandler:
+(BLCNewItemCompletionBlock)completionHandler {
     if (self.accessToken) {
         // only try to get the data if there's an access token
         
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-            // do the network request in the background, so the UI doesn't lock up
-            
-            NSMutableString *urlString = [NSMutableString stringWithFormat:@"https://api.instagram.com/v1/users/self/feed?access_token=%@", self.accessToken];
-            
-            for (NSString *parameterName in parameters) {
-                // for example, if dictionary contains {count: 50}, append `&count=50` to the URL
-                [urlString appendFormat:@"&%@=%@", parameterName, parameters[parameterName]];
-            }
-            
-            NSURL *url = [NSURL URLWithString:urlString];
-            
-            if (url) {
-                NSURLRequest *request = [NSURLRequest requestWithURL:url];
-                
-                NSURLResponse *response;
-                NSError *webError;
-                NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&webError];
-                
-                NSError *jsonError;
-                NSDictionary *feedDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:&jsonError];
-                
-                if (feedDictionary) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        // done networking, go back on the main thread
-                        [self parseDataFromFeedDictionary:feedDictionary fromRequestWithParameters:parameters];
-                    });
-                }
-            }
-        });
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0),
+                       ^{
+                           // do the network request in the background, so the UI doesn't lock up
+                           
+                           NSMutableString *urlString = [NSMutableString
+                                                         stringWithFormat:
+                                                         @"https://api.instagram.com/v1/users/self/feed?access_token=%@",
+                                                         self.accessToken];
+                           
+                           for (NSString *parameterName in parameters) {
+                               // for example, if dictionary contains {count: 50}, append `&count=50`
+                               // to the URL
+                               [urlString
+                                appendFormat:@"&%@=%@", parameterName, parameters[parameterName]];
+                           }
+                           
+                           NSURL *url = [NSURL URLWithString:urlString];
+                           
+                           if (url) {
+                               NSURLRequest *request = [NSURLRequest requestWithURL:url];
+                               
+                               NSURLResponse *response;
+                               NSError *webError;
+                               NSData *responseData =
+                               [NSURLConnection sendSynchronousRequest:request
+                                                     returningResponse:&response
+                                                                 error:&webError];
+                               
+                               if (responseData) {
+                                   NSError *jsonError;
+                                   NSDictionary *feedDictionary =
+                                   [NSJSONSerialization JSONObjectWithData:responseData
+                                                                   options:0
+                                                                     error:&jsonError];
+                                   
+                                   if (feedDictionary) {
+                                       dispatch_async(dispatch_get_main_queue(), ^{
+                                           // done networking, go back on the main thread
+                                           [self parseDataFromFeedDictionary:feedDictionary
+                                                   fromRequestWithParameters:parameters];
+                                           if (completionHandler) {
+                                               completionHandler(nil);
+                                           }
+                                       });
+                                   } else if (completionHandler) {
+                                       dispatch_async(dispatch_get_main_queue(),
+                                                      ^{ completionHandler(jsonError); });
+                                   }
+                               } else if (completionHandler) {
+                                   dispatch_async(dispatch_get_main_queue(),
+                                                  ^{ completionHandler(webError); });
+                               }
+                           }
+                       });
     }
 }
 
-- (void) parseDataFromFeedDictionary:(NSDictionary *) feedDictionary fromRequestWithParameters:(NSDictionary *)parameters {
+- (void)parseDataFromFeedDictionary:(NSDictionary *)feedDictionary
+          fromRequestWithParameters:(NSDictionary *)parameters {
     NSArray *mediaArray = feedDictionary[@"data"];
     
     NSMutableArray *tmpMediaItems = [NSMutableArray array];
@@ -168,36 +216,68 @@
         }
     }
     
-    [self willChangeValueForKey:@"mediaItems"];
-    self.mediaItems = tmpMediaItems;
-    [self didChangeValueForKey:@"mediaItems"];
+    NSMutableArray *mutableArrayWithKVO =
+    [self mutableArrayValueForKey:@"mediaItems"];
+    
+    if (parameters[@"min_id"]) {
+        // This was a pull-to-refresh request
+        
+        NSRange rangeOfIndexes = NSMakeRange(0, tmpMediaItems.count);
+        NSIndexSet *indexSetOfNewObjects =
+        [NSIndexSet indexSetWithIndexesInRange:rangeOfIndexes];
+        
+        [mutableArrayWithKVO insertObjects:tmpMediaItems
+                                 atIndexes:indexSetOfNewObjects];
+    } else if (parameters[@"max_id"]) {
+        // This was an infinite scroll request
+        
+        if (tmpMediaItems.count == 0) {
+            // disable infinite scroll, since there are no more older messages
+            self.thereAreNoMoreOlderMessages = YES;
+        }
+        
+        [mutableArrayWithKVO addObjectsFromArray:tmpMediaItems];
+        
+    } else {
+        [self willChangeValueForKey:@"mediaItems"];
+        self.mediaItems = tmpMediaItems;
+        [self didChangeValueForKey:@"mediaItems"];
+    }
 }
 
-- (void) downloadImageForMediaItem:(BLCMedia *)mediaItem {
+- (void)downloadImageForMediaItem:(BLCMedia *)mediaItem {
     if (mediaItem.mediaURL && !mediaItem.image) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            NSURLRequest *request = [NSURLRequest requestWithURL:mediaItem.mediaURL];
-            
-            NSURLResponse *response;
-            NSError *error;
-            NSData *imageData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            
-            if (imageData) {
-                UIImage *image = [UIImage imageWithData:imageData];
-                
-                if (image) {
-                    mediaItem.image = image;
-                    
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
-                        NSUInteger index = [mutableArrayWithKVO indexOfObject:mediaItem];
-                        [mutableArrayWithKVO replaceObjectAtIndex:index withObject:mediaItem];
-                    });
-                }
-            } else {
-                NSLog(@"Error downloading image: %@", error);
-            }
-        });
+        dispatch_async(
+                       dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                           NSURLRequest *request =
+                           [NSURLRequest requestWithURL:mediaItem.mediaURL];
+                           
+                           NSURLResponse *response;
+                           NSError *error;
+                           NSData *imageData =
+                           [NSURLConnection sendSynchronousRequest:request
+                                                 returningResponse:&response
+                                                             error:&error];
+                           
+                           if (imageData) {
+                               UIImage *image = [UIImage imageWithData:imageData];
+                               
+                               if (image) {
+                                   mediaItem.image = image;
+                                   
+                                   dispatch_async(dispatch_get_main_queue(), ^{
+                                       NSMutableArray *mutableArrayWithKVO =
+                                       [self mutableArrayValueForKey:@"mediaItems"];
+                                       NSUInteger index =
+                                       [mutableArrayWithKVO indexOfObject:mediaItem];
+                                       [mutableArrayWithKVO replaceObjectAtIndex:index
+                                                                      withObject:mediaItem];
+                                   });
+                               }
+                           } else {
+                               NSLog(@"Error downloading image: %@", error);
+                           }
+                       });
     }
 }
 
